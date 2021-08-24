ea8363b4dc7f5c209fc31b78db710b601d07f4b8
shivank goyal <goyal.shivank790@gmail.com>
2021-08-24 23:52:19 +0530

    react router added to navlinks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import Mainnav from './shared/Components/Mainnav';
+import Auth from "./shared/Auth/Auth"
+import { BrowserRouter as Router, Route , Switch, Redirect } from "react-router-dom";
 
 const App = () =>{
   return(
-    <Mainnav></Mainnav>
+    <Router>
+     <Switch>
+      <Route path="/Auth" exact><Auth/></Route> 
+        <div>
+      <Route path="/" exact><Mainnav></Mainnav></Route>
+      </div>
+      <Redirect to="/" />
+    </Switch>
+    </Router>
   )
 }
 export default App;
