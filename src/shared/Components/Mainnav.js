9b7e7534b49bb786ab960d514fca338c8d0a8aae
shivank goyal <goyal.shivank790@gmail.com>
2021-08-26 22:04:37 +0530

    added links to all navbar components

--- src/shared/Components/Mainnav.js
+++ src/shared/Components/Mainnav.js
@@ -2,11 +2,12 @@ import React from "react"
 import "./Mainnav.css"
 import Navbar from "./Navbar"
 import Searchbar from "./Searchbar"
+import {Link} from "react-router-dom";
 
 const Mainnav = () =>{
 return(
 <div className="main-header"> 
-    <h1 className="logo">Buy&Sell</h1>
+    <h1 className="logo"><Link to="/">Buy&Sell</Link></h1>
     <Searchbar></Searchbar>
     <Navbar></Navbar>
   
--- src/shared/Components/Navbar.js
+++ src/shared/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Navbar.css"
 import {NavLink} from 'react-router-dom'
+import Avatar from "./Avatar";
 
 const Navbar = () =>{
     return(
     <ul className="navlinks">
+        <li><Avatar></Avatar></li>
         <li><NavLink to="/Auth"><u>Login</u></NavLink> </li>
-        <li><button className="sellbtn">SELL +</button></li>
+        <li><button className="sellbtn"><NavLink to="/Sell">SELL +</NavLink></button></li>
     </ul>);
 }
 
