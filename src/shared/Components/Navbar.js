fbfdf8009456b29d62f51510ae8ebb532976c21a
shivank goyal <goyal.shivank790@gmail.com>
2021-08-28 19:33:52 +0530

    made headers and footers responsive

--- src/shared/Components/Navbar.js
+++ src/shared/Components/Navbar.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css"
 import {NavLink} from 'react-router-dom'
 import Avatar from "./Avatar";
-
+import {AuthContext} from "../Authcontext"
 const Navbar = () =>{
+    const auth = useContext(AuthContext);
     return(
     <ul className="navlinks">
         <li><Avatar></Avatar></li>
-        <li><NavLink to="/Auth"><u>Login</u></NavLink> </li>
+        {!auth.isLoggedIn && (
+        <li><NavLink to="/Auth"><u>Login</u></NavLink> </li>)}
+        {auth.isLoggedIn && (<li  onClick={auth.logout}><u>Logout</u></li>)}
         <li><button className="sellbtn"><NavLink to="/Sell">SELL +</NavLink></button></li>
     </ul>);
 }
