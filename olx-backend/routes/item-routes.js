b1b6ffc1b0753e3b15d51da4fd591374375c3505
shivank goyal <goyal.shivank790@gmail.com>
2021-08-29 22:53:18 +0530

    added new routes

--- olx-backend/routes/item-routes.js
+++ olx-backend/routes/item-routes.js
@@ -1,33 +1,25 @@
 const express = require("express");
-
+const itemcontrollers = require("../Controllers/item-controllers")
 const router = express.Router();
 
-const Dummyitems = [
-    {
-        id : "p1",
-        name : "BMW",
-        sellprice : 760000,
-        age : "2017 mdoel",
-        description : "2400km moved",
-        location : "agra",
-        creator : "u1"
+// const Dummyitems = [
+//     {
+//         id : "p1",
+//         name : "BMW",
+//         sellprice : 760000,
+//         age : "2017 mdoel",
+//         description : "2400km moved",
+//         location : "agra",
+//         creator : "u1"
         
-    }
-]
+//     }
+// ]
 
-router.get('/',);
-
-router.get("/:pid/item",(req,res,next) =>{
-    const itemid = req.params.pid;
-    const answer = Dummyitems.find(item => {return item.id === itemid});
-    res.json({answer : answer})
-}); 
-
-//getitemby userid
-router.get("/:uid/usersitem",(req,res,next) =>{
-    const userid = req.params.uid;
-    const answer = Dummyitems.find(user => { return user.creator === userid});
-    res.json({answer : answer})
-});
+router.get('/',itemcontrollers.getallitems);
+router.get("/:pid/item",itemcontrollers.getitembyid); 
+router.get("/:uid/useritems",itemcontrollers.getitemsbyuserid);
+router.post("/sell",itemcontrollers.createitem);
+router.patch("/:pid/edititem",itemcontrollers.updateitem);
+router.delete("/:pid/delete",itemcontrollers.deleteitem);
 
 module.exports = router;
--- olx-backend/routes/user-routes.js
+++ olx-backend/routes/user-routes.js
@@ -1,20 +1,17 @@
 const express = require("express");
-
+const userscontrollers = require("../Controllers/users-controllers")
 const router = express.Router();
 
-const users = [{
-    id : "u1",
-    name : "shivank",
-    email: "goyal.shivank790@gmail.com",
-    password : "shivank119",
-    mobile : "7906558590",
+// const users = [{
+//     id : "u1",
+//     name : "shivank",
+//     email: "goyal.shivank790@gmail.com",
+//     password : "shivank119",
+//     mobile : "7906558590",
+// }]
 
-}]
-
-router.get('/:uid',(req,res,next) =>{
-const userid = req.params.uid;
-const answer = users.filter(user => user.id === userid);
-res.json(answer);
-});
+router.get('/:uid',userscontrollers.getuserbyid);
+router.post("/login",userscontrollers.login);
+router.post("/signup",userscontrollers.signup);
 
 module.exports = router;
