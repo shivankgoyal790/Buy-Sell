3a320d14a31b5c4bf68a3c761b60b1546eba0e4b
shivank goyal <goyal.shivank790@gmail.com>
2021-08-29 22:50:29 +0530

    REST api for users

---
+++ olx-backend/Controllers/users-controllers.js
@@ -0,0 +1,93 @@
+const e = require("express");
+const mongoose = require("mongoose");
+const Items = require("../models/item-model");
+const Users = require("../models/user-model");
+
+const users = [{
+    id : "u1",
+    name : "shivank",
+    email: "goyal.shivank790@gmail.com",
+    password : "shivank119",
+    mobile : "7906558590",
+}]
+
+const getuserbyid = (req,res,next) =>{
+const userid = req.params.uid;
+const answer = users.filter(user => user.id === userid);
+res.json(answer);
+};
+
+// const login = (req,res,next) =>{
+//     const {email,password} = req.body;
+//     const user = users.find(emailid => emailid.email === email);
+//     if(user && user.password === password){
+//         res.status(200).json("you are logged");
+//     }
+//     else{
+//         res.status(400).json("check credentials")
+//     }     
+// }
+
+const login = async (req,res,next) =>{
+    const {email,password} = req.body;
+    let usercheck
+    try{
+        usercheck = await Users.findOne({email:email})
+    }catch(err){
+        console.log(err);
+        res.status(400).json("try after some time");
+    }
+
+    if(!usercheck){
+        res.status(400).json("invalid email")
+    }
+    else{
+        if(usercheck.password !== password){
+            res.status(400).json("invalid password");
+        }
+    }
+    res.json("you are logged",{user:usercheck.toObject({getters:true})})
+
+}
+// const signup = (req,res,next) =>{
+//   const {name ,email,password,mobile} = req.body;
+//   const newuser = {
+//       name,
+//       email,
+//       password,
+//       mobile
+//   }
+//   users.push(newuser);
+//   res.status(200).json({newuser});
+// }
+
+const signup = async(req,res,next) =>{
+    const {name ,email,password,mobile} = req.body;
+    const newuser = await new Users(
+        {
+        name:name,
+        email:email,
+        password,
+        mobile,
+        items : []
+        }
+    )
+
+    try{
+        if(!newuser.password || newuser.name === "" || newuser.email === "" ){
+            throw new Error("check credential")
+        }
+        else{
+            await newuser.save();
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(404).json("cannot signup");
+    }
+
+    res.json("you are signed in");
+} 
+exports.getuserbyid =getuserbyid;
+exports.login = login;
+exports.signup = signup;
