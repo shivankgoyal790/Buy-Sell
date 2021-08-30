573d84cfbb4170c015ba07f7a2c739bc6bfc3e20
shivank goyal <goyal.shivank790@gmail.com>
2021-08-30 22:56:16 +0530

    connect to mongoose client

--- olx-backend/app.js
+++ olx-backend/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const userroutes = require("./routes/user-routes");
-const itemroutes = require("./routes/item-routes")
+const itemroutes = require("./routes/item-routes");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
 
 app.use("/users",userroutes);
 app.use("/api",itemroutes);
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});         
+
+mongoose.connect("mongodb+srv://shivank:shivank@cluster0.e3ldd.mongodb.net/buy&sell?retryWrites=true&w=majority")
+.then(()=>{
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });        
+}).catch(err => {
+
+  console.log(err);
+}); 
 
 
